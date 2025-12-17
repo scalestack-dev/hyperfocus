bfbd5411e27786e4cc1503a9418cf304d0f21a99
John Doe <johndoe@example.com>
2025-12-17 20:31:16 +0100

    Fix: Rename contexts to contexts_temp

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { LanguageProvider } from './contexts/LanguageContext';
-
+import { LanguageProvider } from './contexts_temp/LanguageContext';
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
