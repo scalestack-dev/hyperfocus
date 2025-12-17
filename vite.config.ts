3ff79dbcc2828fb445554447022cf3f2a60bf679
John Doe <johndoe@example.com>
2025-12-17 18:43:34 +0100

    Fix blank page with base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  // On utilise process.cwd() directement (ou (process as any).cwd() si TS rale)
+  const env = loadEnv(mode, process.cwd(), '');
+
   return {
+    base: "./",  // <--- LIGNE CRITIQUE AJOUTÉE ICI
     plugins: [react()],
     define: {
       // Shim process.env for existing code compatibility
